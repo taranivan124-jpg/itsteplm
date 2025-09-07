3d1690f024caa08dadc1c17af77023119d319054
201-12 <szeltuhin713@gmail.com>
2025-09-07 17:47:24 +0300

    смарагдові класи

--- 111.py
+++ 111.py
@@ -1,3 +1,34 @@
 print("смарагдове небо")
 for i in range(10):
     print("смарагдове небо")
+# class EMERALD_SKY:
+#     def data(self,name, sex):
+#         self.age = 7
+#         print("чекає на завтра")
+#         print(name, self.age, sex)
+# es1 = EMERALD_SKY()
+# es1.data("skebob", "idk")
+
+
+
+# class EMERALD_SKY:
+#     def __init__(self,name, sex):
+#         self.age = 7
+#         print("чекає на завтра")
+#         print(name, self.age, sex)
+#
+#
+# es1 = EMERALD_SKY("barabaka","female")
+
+# def chivapchichi(name):
+#     print(name, "chupep")
+
+class EMERALD_SKY:
+    def __init__(self,fruit = "apple"):
+        self.f = fruit
+    def frrrrruit(self, price):
+        print(price)
+es1 = EMERALD_SKY()
+print(es1.f)
+es1.frrrrruit(67)
+
